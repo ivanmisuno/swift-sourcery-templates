db5e9eba2e6dc3e655f40772fab8e9394ac9ef6c
i.misuno <ivan.misuno@albelli.com>
2018-07-18 17:01:19 +0200

    `Mocks.swittemplate`: add method handlers.

--- tests/SwiftSourceryTemplates/SwiftSourceryTemplatesTests/Mocks/Mocks.generated.swift
+++ tests/SwiftSourceryTemplates/SwiftSourceryTemplatesTests/Mocks/Mocks.generated.swift
@@ -15,22 +15,22 @@ import RxTest
 // MARK: - AlbumPageSizeProviding
 class AlbumPageSizeProvidingMock: AlbumPageSizeProviding {
 
-            // MARK: - pageSize
+    // MARK: - pageSize
 
 }
 
 // MARK: - ErrorPopoverBuildable
 class ErrorPopoverBuildableMock: ErrorPopoverBuildable {
 
-            // MARK: - buildPopoverPresenter<T>
-
+    // MARK: - buildPopoverPresenter<T>(title: String, buttons: [(title: String, identifier: T, handler: ()->())])
+    var buildPopoverPresenterHandler: ((_ title: String, _ buttons: [(title: String, identifier: T, handler: ()->())]) -> AnyErrorPopoverPresentable<T>)? = nil
 }
 
 // MARK: - ErrorPopoverBuildableRawRepresentable
 class ErrorPopoverBuildableRawRepresentableMock: ErrorPopoverBuildableRawRepresentable {
 
-            // MARK: - buildPopoverPresenter<T: RawRepresentable>
-
+    // MARK: - buildPopoverPresenter<T: RawRepresentable>(title: String, buttons: [(title: String, identifier: T, handler: ()->())])
+    var buildPopoverPresenterHandler: ((_ title: String, _ buttons: [(title: String, identifier: T, handler: ()->())]) -> AnyErrorPopoverPresentableRawRepresentable<T>)? = nil
 }
 
 // MARK: - ErrorPopoverPresentable
@@ -39,8 +39,8 @@ class ErrorPopoverPresentableMock<TypeEventType>: ErrorPopoverPresentable {
     typealias EventType = TypeEventType
 
 
-            // MARK: - show
-
+    // MARK: - show(relativeTo positioningRect: NSRect, of positioningView: NSView, preferredEdge: NSRectEdge)
+    var showHandler: ((_ positioningRect: NSRect, _ positioningView: NSView, _ preferredEdge: NSRectEdge) -> Observable<EventType>)? = nil
 }
 
 // MARK: - ErrorPopoverPresentableRawRepresentable
@@ -49,22 +49,22 @@ class ErrorPopoverPresentableRawRepresentableMock<TypeEventType>: ErrorPopoverPr
     typealias EventType = TypeEventType
 
 
-            // MARK: - show
-
+    // MARK: - show(relativeTo positioningRect: NSRect, of positioningView: NSView, preferredEdge: NSRectEdge)
+    var showHandler: ((_ positioningRect: NSRect, _ positioningView: NSView, _ preferredEdge: NSRectEdge) -> Observable<EventType>)? = nil
 }
 
 // MARK: - ExifImageAttributeProviding
 class ExifImageAttributeProvidingMock: ExifImageAttributeProviding {
 
-            // MARK: - dateTaken
-
+    // MARK: - dateTaken(fileUrl: URL)
+    var dateTakenHandler: ((_ fileUrl: URL) -> Date?)? = nil
 }
 
 // MARK: - ImageAttributeProviding
 class ImageAttributeProvidingMock: ImageAttributeProviding {
 
-            // MARK: - imagePixelSize
-
+    // MARK: - imagePixelSize(source: UploadAPI.LocalFile.Source)
+    var imagePixelSizeHandler: ((_ source: UploadAPI.LocalFile.Source) throws -> CGSize)? = nil
 }
 
 // MARK: - MutableTipsManaging
@@ -74,37 +74,37 @@ class MutableTipsManagingMock: MutableTipsManaging {
 // MARK: - MutableUploadProgressing
 class MutableUploadProgressingMock: MutableUploadProgressing {
 
-            // MARK: - progress
+    // MARK: - progress
 
 
-            // MARK: - setInputFiles
-
-            // MARK: - uploadIdRetrieved
-
-            // MARK: - filePartsCreated
-
-            // MARK: - filePartProgressed
-
-            // MARK: - downloadUrlsRetrieved
-
-            // MARK: - fileProgress
-
+    // MARK: - setInputFiles(localFiles: [UploadAPI.LocalFile])
+    var setInputFilesHandler: ((_ localFiles: [UploadAPI.LocalFile]) -> Void)? = nil
+    // MARK: - uploadIdRetrieved(localFile: UploadAPI.LocalFile, uploadId: UploadAPI.UploadId)
+    var uploadIdRetrievedHandler: ((_ localFile: UploadAPI.LocalFile, _ uploadId: UploadAPI.UploadId) -> Void)? = nil
+    // MARK: - filePartsCreated(uploadId: UploadAPI.UploadId, fileParts: [FilePart])
+    var filePartsCreatedHandler: ((_ uploadId: UploadAPI.UploadId, _ fileParts: [FilePart]) -> Void)? = nil
+    // MARK: - filePartProgressed(uploadId: UploadAPI.UploadId, filePart: FilePart, progress: RxProgress)
+    var filePartProgressedHandler: ((_ uploadId: UploadAPI.UploadId, _ filePart: FilePart, _ progress: RxProgress) -> Void)? = nil
+    // MARK: - downloadUrlsRetrieved()
+    var downloadUrlsRetrievedHandler: (() -> Void)? = nil
+    // MARK: - fileProgress(_ source: UploadAPI.LocalFile.Source)
+    var fileProgressHandler: ((UploadAPI.LocalFile.Source) -> Observable<RxProgress>)? = nil
 }
 
 // MARK: - ThumbCreating
 class ThumbCreatingMock: ThumbCreating {
 
-            // MARK: - createThumbImage
-
-            // MARK: - createThumbJpegData
-
+    // MARK: - createThumbImage(for pictureUrl: URL, fitting size: CGSize)
+    var createThumbImageHandler: ((_ pictureUrl: URL, _ size: CGSize) throws -> NSImage)? = nil
+    // MARK: - createThumbJpegData(for pictureUrl: URL, fitting size: CGSize, compression: Double)
+    var createThumbJpegDataHandler: ((_ pictureUrl: URL, _ size: CGSize, _ compression: Double) throws -> Data)? = nil
 }
 
 // MARK: - TipsManagerBuilding
 class TipsManagerBuildingMock: TipsManagerBuilding {
 
-            // MARK: - build
-
+    // MARK: - build()
+    var buildHandler: (() -> TipsManaging & MutableTipsManaging)? = nil
 }
 
 // MARK: - TipsManaging
@@ -114,9 +114,9 @@ class TipsManagingMock: NSObject, TipsManaging {
 // MARK: - UploadProgressing
 class UploadProgressingMock: UploadProgressing {
 
-            // MARK: - progress
+    // MARK: - progress
 
 
-            // MARK: - fileProgress
-
+    // MARK: - fileProgress(_ source: UploadAPI.LocalFile.Source)
+    var fileProgressHandler: ((UploadAPI.LocalFile.Source) -> Observable<RxProgress>)? = nil
 }
